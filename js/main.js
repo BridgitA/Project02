65c83ed24d717afde4857c156533eee41468ed32
Bridgit Arika <bridgitmob@gmail.com>
2020-08-16 19:50:08 -0500

    Added ajax code for the api key url

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ $(document).ready(()=>{
     $("#submit").click(()=>{
         let userInput = $("#search").val()
         alert(userInput)
+        $.ajax({
+            url:'x2Ew2uhWSu0zAcyfcaUftSH6t2m99mkn'
+        });
     });
     
 });
