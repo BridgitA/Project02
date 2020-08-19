c40273c580ef3ada3034130a7ffc317552852db2
Bridgit Arika <bridgitmob@gmail.com>
2020-08-19 00:39:37 -0500

    added the api key

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-$(document).ready(()=>{
-    $("#submit").click(()=>{
-        let userInput = $("#search").val()
-        alert(userInput)
-    });
+
+        var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=x2Ew2uhWSu0zAcyfcaUftSH6t2m99mkn&limit=30");
+
+        xhr.done(function(response) { console.log("success got data", response); });
+
+        var giffs = response.data
+
+        for(i in giffs){
+            $('.inner').append("<img src='"+giffs[i].images.original.url+"'>")
+        }
     
-});
