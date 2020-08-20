9b692ce2da969c0971538d7f0519ee372f426954
Bridgit Arika <bridgitmob@gmail.com>
2020-08-20 11:52:46 -0500

    working on the get data function

--- js/main.js
+++ js/main.js
@@ -1,17 +1,20 @@
 
 function getData(){
-    var input = $("#search").val()
 
-    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=x2Ew2uhWSu0zAcyfcaUftSH6t2m99mkn&limit=30");
+    var input = $("#search").val()
+    
+
+    var xhr = $.get("https://api.giphy.com/v1/gifs/search?api_key=rLYdhrgwH4Bt3A5luRaleol4AHs891pb&q=dogs&limit=30");
 
     xhr.done(function(response) { console.log("success got data", response); 
 
     var giffs = response.data
 
     for(i in giffs){
-        $('.inner').append("<img src='"+giffs[i].images.original.url+"'>")
+        $('.inner').append("<img src="+giffs[i].images.original.url+">")
     }
-         });
+
+});
 
 }
 
