13e79abc90107b7fc17942f2cde4be53866f73a0
Bridgit Arika <bridgitmob@gmail.com>
2020-08-21 13:25:47 -0500

    added styling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-function getData(){
+function getData() {
     $(".inner").empty();
     var input = $('#search').val();
-    $.get('http://api.giphy.com/v1/gifs/search?q='+input+'&api_key=rLYdhrgwH4Bt3A5luRaleol4AHs891pb&limit=30', function(response) {
-       
-        for(i = 0; response.data.length; i++){
-            $('.inner').append("<img src="+response.data[i].images.original.url+">")
+    $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=rLYdhrgwH4Bt3A5luRaleol4AHs891pb&limit=30', function (response) {
+
+        for (i = 0; response.data.length; i++) {
+            $('.inner').append("<img src=" + response.data[i].images.original.url + ">")
         }
-    }) 
+    })
 }
 
