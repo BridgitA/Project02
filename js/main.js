87b1ce28b151d896530b6de090a5edce770b2df4
Bridgit Arika <bridgitmob@gmail.com>
2020-08-23 12:58:17 -0500

    added a flex child

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function getData() {
     $.get('http://api.giphy.com/v1/gifs/search?q=' + input + '&api_key=rLYdhrgwH4Bt3A5luRaleol4AHs891pb&limit=30', function (response) {
 
         for (i = 0; response.data.length; i++) {
-            $('.inner').append("<img src=" + response.data[i].images.original.url + ">")
+            $('.inner').append("<div class='flex-child'><img src='" + response.data[i].images.original.url + "'></div>")
         }
     })
 }
